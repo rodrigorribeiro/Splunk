3b2191aedb1244825152b7027dcae94985afcf3d
Rodrigo Ribeiro <silva.rrs@gmail.com>
2020-08-11 11:20:48 -0300

    Update command_v2.py

--- commands/bin/command_v2.py
+++ commands/bin/command_v2.py
@@ -1,6 +1,4 @@
 import sys
-import base64
-import zlib
 from splunklib.searchcommands import dispatch, StreamingCommand, Configuration
 
 @Configuration()
@@ -8,8 +6,7 @@ class SampleCommand(StreamingCommand):
     def stream(self, records):
         for record in records:
             each_payload = record['payload']
-            b64 = base64.b64decode(each_payload)
-            record["out"] = zlib.decompress(b64).decode('utf-8')
+            # DO SOMETHING HERE WITH EACH STRING
             yield record
 
 if __name__ == "__main__":
