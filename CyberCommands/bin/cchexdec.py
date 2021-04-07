b5a1d6689592503f044dbc634762328ae75d7d82
Rodrigo Ribeiro <silva.rrs@gmail.com>
2021-04-06 23:00:36 -0300

    Add files via upload

---
+++ CyberCommands/bin/cchexdec.py
@@ -0,0 +1,29 @@
+import sys
+import base64
+from splunklib.searchcommands import dispatch, StreamingCommand, Configuration, Option, validators
+
+@Configuration()
+class CyberCommands(StreamingCommand):
+
+    field = Option(
+        doc='''
+        **Syntax:** **field=***<fieldname>*
+        **Description:** Name of the field which contains the value''',
+        require=False, validate=validators.Fieldname())
+
+    def stream(self, records):
+        # check if the field value was specified
+        if self.field == None:
+            raise Exception("The field must be specified!")
+        else:
+            for record in records:
+                # get value from field
+                payload = record[self.field]
+                # decode payload
+                payload_dec = bytes.fromhex(payload)
+                # creating a new var using suffix _dec
+                record[self.field+"_dec"] = payload_dec.decode('utf-8')
+                yield record
+
+if __name__ == "__main__":
+    dispatch(CyberCommands, sys.argv, sys.stdin, sys.stdout, __name__)
